f2abc44a21bb1cead043933be499e3d581664a52
Yoga Faodiansyah <yoga.faodiansyah@polyrific.com>
2023-06-27 04:54:52 +0000

    Updated outputs.tf

--- infra-eco/storage/outputs.tf
+++ infra-eco/storage/outputs.tf
@@ -5,3 +5,11 @@ output "functionStorageName" {
 output "functionStorageKey" {
   value = azurerm_storage_account.functionStorage.primary_access_key
 }
+
+output "adlStorageId" {
+  value = azurerm_storage_account.storageAccont.id
+}
+
+output "adlContainerId" {
+  value = azurerm_storage_data_lake_gen2_filesystem.ADLv2.id
+}
